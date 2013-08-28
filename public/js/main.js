667eda17ffd9e8926f37491727b675864f671b01
Andrew Kostka <akostka@gunnjerkens.com>
2013-08-28 14:43:17 -0700

    trends graph

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,18 @@
-//Line graph data
-var data = {
-	labels : ["Read","Retain"],
+//Effectiveness graph data
+var barData = {
+	labels : ["With Pictures","Without"],
 	datasets : [
-		{
-			fillColor : "rgba(209,85,77,0.5)",
-			strokeColor : "rgba(209,85,77,1)",
-			pointStrokeColor : "#fff",
-			data : [95,75]
-		},
 		{
 			fillColor : "rgba(54,129,200,0.5)",
 			strokeColor : "rgba(54,129,200,1)",
 			pointStrokeColor : "#fff",
-			data : [25,10]
+			data : [95,35]
+		},
+		{
+			fillColor : "rgba(209,85,77,0.5)",
+			strokeColor : "rgba(209,85,77,1)",
+			pointStrokeColor : "#fff",
+			data : [65,20]
 		}
 	]
 };
@@ -25,6 +25,35 @@ var options = {
 	scaleFontSize: 20,
 	barValueSpacing: 120,
 	barDatasetSpacing: 10,
+	animationEasing: 'easeInOutQuint'
+};
+
+//Trends graph data
+var trendData = {
+	labels : ["2009","2010","2011","2012","2013"],
+	datasets : []
+};
+var twitterData = {
+	fillColor : "rgba(54,129,200,0.5)",
+	strokeColor : "rgba(54,129,200,1)",
+	pointColor : "rgba(54,129,200,1)",
+	pointStrokeColor : "#fff",
+	data : [10,20,30,40,50]
+};
+var googleData = {
+	fillColor : "rgba(209,85,77,0.5)",
+	strokeColor : "rgba(209,85,77,1)",
+	pointColor : "rgba(209,85,77,1)",
+	pointStrokeColor : "#fff",
+	data : [30,40,50,60,70]
+};
+var options = {
+	scaleOverride: true,
+	scaleSteps: 10,
+	scaleStepWidth: 10,
+	scaleStartValue: 0,
+	scaleFontFamily: "'Open Sans',sans-serif",
+	scaleFontSize: 20,
 	animationEasing: 'easeInOutQuint'
 };
 
@@ -41,16 +70,43 @@ jQuery(function($){
 		});
 	}, { offset: 'bottom-in-view' });
 
+	//Effectiveness chart
 	$('a.makeChart').on('click', function(e){	
 		e.preventDefault();
 		$('.makeChart').fadeOut(500,function(){
 			$('#whyEffective .chrome-frame p').addClass('blur');
 			
 			//Make Chart
-			$('#whyEffective .chrome-frame').append('<canvas id="readChart" width="'+$('#whyEffective .chrome-frame').width()+'" height="400"></canvas><ul class="legend"><li class="red">With Pictures</li><li class="blue">Without</li></ul>');
+			$('#whyEffective .chrome-frame .chart').html('<canvas id="readChart" width="'+$('#whyEffective .chrome-frame .chart').width()+'" height="400"></canvas><ul class="legend"><li class="blue">Read</li><li class="red">Retain</li></ul>');
 			var ctx = $('#readChart').get(0).getContext('2d');
-			var newChart = new Chart(ctx).Bar(data, options);
+			var newChart = new Chart(ctx).Bar(barData, options);
 		});
+		return false;
+	});
+
+	//Trends chart
+	function trendChart(datasets) {
+		trendData.datasets = datasets;
+		var ctx = $('#trendChart').get(0).getContext('2d');
+		var newChart = new Chart(ctx).Line(trendData, options);
+	}
+	$('#whoUses .chart').html('<canvas id="trendChart" width="'+$('#whoUses .chart').width()+'" height="400"></canvas>');
+	var datasets = [googleData,twitterData];
+	trendChart(datasets);
+
+	$('#whoUses .trendSort a.google').on('click', function(e){
+		e.preventDefault();
+		trendChart([googleData]);
+		return false;
+	});
+	$('#whoUses .trendSort a.twitter').on('click', function(e){
+		e.preventDefault();
+		trendChart([twitterData]);
+		return false;
+	});
+	$('#whoUses .trendSort a.plus').on('click', function(e){
+		e.preventDefault();
+		trendChart([googleData,twitterData]);
 		return false;
 	});
 });
