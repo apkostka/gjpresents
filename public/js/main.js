35b4467c78700356ebd40dae784e233e3013be48
Andrew Kostka <akostka@gunnjerkens.com>
2013-08-28 11:47:08 -0700

    bar graph

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,30 @@
 //Line graph data
 var data = {
-	labels : ["1","2","3"],
+	labels : ["Read","Retain"],
 	datasets : [
 		{
 			fillColor : "rgba(209,85,77,0.5)",
 			strokeColor : "rgba(209,85,77,1)",
-			pointColor : "rgba(209,85,77,1)",
 			pointStrokeColor : "#fff",
-			data : [65,59,90]
+			data : [95,75]
 		},
 		{
 			fillColor : "rgba(54,129,200,0.5)",
 			strokeColor : "rgba(54,129,200,1)",
-			pointColor : "rgba(54,129,200,1)",
 			pointStrokeColor : "#fff",
-			data : [28,48,40]
+			data : [25,10]
 		}
 	]
 };
-var lineOptions = {
+var options = {
 	scaleOverride: true,
 	scaleSteps: 10,
 	scaleStepWidth: 10,
 	scaleStartValue: 0,
 	scaleFontFamily: "'Open Sans',sans-serif",
+	scaleFontSize: 20,
+	barValueSpacing: 120,
+	barDatasetSpacing: 10,
 	animationEasing: 'easeInOutQuint'
 };
 
@@ -46,9 +47,9 @@ jQuery(function($){
 			$('#whyEffective .chrome-frame p').addClass('blur');
 			
 			//Make Chart
-			$('#whyEffective .chrome-frame').append('<canvas id="readChart" width="'+$('#whyEffective .chrome-frame').width()+'" height="400"></canvas><ul class="legend"><li class="blue">Read</li><li class="red">Retain</li></ul>');
+			$('#whyEffective .chrome-frame').append('<canvas id="readChart" width="'+$('#whyEffective .chrome-frame').width()+'" height="400"></canvas><ul class="legend"><li class="red">With Pictures</li><li class="blue">Without</li></ul>');
 			var ctx = $('#readChart').get(0).getContext('2d');
-			var newChart = new Chart(ctx).Line(data,lineOptions);
+			var newChart = new Chart(ctx).Bar(data, options);
 		});
 		return false;
 	});
